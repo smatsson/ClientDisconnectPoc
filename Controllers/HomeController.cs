29ef8d13eb4246627f88c4ece4b2e70cfe70798a
Stefan Matsson <stefan.matsson@advisesolutions.se>
2018-09-19 10:15:59 +0200

    POC for client disconnects during reads

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            await Task.Delay(5000);
+
+            if (HttpContext.RequestAborted.IsCancellationRequested)
+            {
+                // Won't ever fire even if the client disconnected during the delay
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CancellationToken token)
+        {
+            await ClientDisconnectGuard.ExecuteAsync(() => ReadDataFromClient(HttpContext), HttpContext.RequestAborted);
+
+            if (token.IsCancellationRequested)
+            {
+                // Yep it worked if the client disconnected during reading data from the client
+            }
+
+            return View();
+        }
+
+        private static async Task ReadDataFromClient(HttpContext context)
+        {
+            var bytesRead = 0;
+            do
+            {
+                if (context.RequestAborted.IsCancellationRequested)
+                {
+                    Logger.WriteLog(true);
+                    break;
+                }
+
+                var buffer = new byte[1];
+                bytesRead = await context.Request.Body.ReadAsync(buffer, 0, 1, context.RequestAborted);
+            }
+            while (bytesRead != 0);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+    public static class Logger
+    {
+        public static void WriteLog(bool tokenWasCancelled)
+        {
+            File.WriteAllText(@"c:\temp\log.txt", DateTime.Now.ToString() + ": " + (tokenWasCancelled ? "Was cancelled" : "Was NOT cancelled"));
+        }
+    }
+}
